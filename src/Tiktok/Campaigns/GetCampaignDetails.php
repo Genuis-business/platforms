2afa604489331882d8cca2e5198bafaa6b33f6c4
Mohammed Hamid <hamoda.dev@gmail.com>
2025-01-22 06:59:59 +0200

    get campaign details

---
+++ src/Tiktok/Campaigns/GetCampaignDetails.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Bian\Platforms\Tiktok\Campaigns;
+
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Traits\Body\HasJsonBody;
+
+class GetCampaignDetails extends Request implements HasBody
+{
+    use HasJsonBody;
+
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        private string $advertiserId,
+        private string $campaignId
+    ) {}
+
+    public function resolveEndpoint(): string
+    {
+        return '/campaign/get';
+    }
+
+    protected function defaultBody(): array
+    {
+        return [
+            'advertiser_id' => $this->advertiserId,
+            'campaign_ids' => [$this->campaignId],
+        ];
+    }
+} 
